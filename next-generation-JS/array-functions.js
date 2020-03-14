1b26fe67a6346519058f1473e2f6311dc606ef28
Ulder Carrilho Júnior <uldercarrilho@gmail.com>
2020-03-14 13:25:11 -0300

    21. Refreshing Array Functions

---
+++ next-generation-JS/array-functions.js
@@ -0,0 +1,8 @@
+// Section 2: Refresing Next Generation JavaScript
+// 21. Refreshing Array Functions
+
+const numbers = [1, 2, 3];
+const doubled = numbers.map((num) => num * 2);
+
+console.log(numbers);
+console.log(doubled);
